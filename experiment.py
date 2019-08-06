c4e967f064e0d1a5f226666b9908b5386e4e9c89
darkanita <amlopezmo@unal.edu.co>
2019-08-06 02:34:59 -0500

    Preprocessing

--- experiment.py
+++ experiment.py
@@ -7,8 +7,8 @@ app_id = 'aBCYHqNarl95prQ6M6RN'
 app_code = 'MGGRNCaXh4WHjFgnef7SlA'
 
 # Conection to AWS S3
-aws_access_key_id='AKIATU6OZUAMMCHEC24U'
-aws_secret_access_key='y/4MfZRwG7JI4gMPQJHJi5petuWgYmNl0U46ux6c'
+aws_access_key_id='YOUR KEY'
+aws_secret_access_key='YOUR KEY'
 
 bucket = 'darkanita'
 key = 'Safety_GPS1.csv'
@@ -17,13 +17,13 @@ urlDataSet = 'https://darkanita.s3-sa-east-1.amazonaws.com/Safecity+Reports+-+28
 dataSet = load_data(urlDataSet)
 print(dataSet.shape)
 dataSet = format_date(dataSet,'INCIDENT DATE')
-dataSet = drop_duplicates(dataSet,['INCIDENT TITLE', 'INCIDENT DATE', 'LOCATION', 'DESCRIPTION','CATEGORY', 'LATITUDE', 'LONGITUDE', 'More Info'])
-print(dataSet.shape)
 dataSet = drop_duplicates(dataSet,['#'])
 print(dataSet.shape)
-dataSet,problems = add_data_location(dataSet,app_id,app_code)
+dataSet = drop_duplicates(dataSet,['INCIDENT TITLE', 'INCIDENT DATE', 'LOCATION', 'DESCRIPTION','CATEGORY', 'LATITUDE', 'LONGITUDE', 'More Info'])
 print(dataSet.shape)
 dataSet,category = create_category_columns(dataSet)
+print(dataSet.shape)
+dataSet,problems = add_data_location(dataSet,app_id,app_code)
 print(dataSet.shape)
 print(dataSet.head())
 obj = upload_data(dataSet,bucket,key,aws_access_key_id,aws_secret_access_key)
