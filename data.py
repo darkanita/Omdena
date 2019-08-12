07e79b1305a26a95b2ea170c6bb8b21aa2d85f4b
darkanita <amlopezmo@unal.edu.co>
2019-08-12 09:49:00 -0500

    Preprocessing

--- data.py
+++ data.py
@@ -194,14 +194,9 @@ def normalize_text(data,column='INCIDENT TITLE'):
     for row in range(len(data[column])):
         try:
             data[column+' SENTENCES'] = str(sent_tokenize(data[column][row]))
-            print('OK')
-            print(data[column][row])
         except Exception as e:
             print(str(e))
             print(data[column][row])
-            i += 1
-            if i == 10:
-                sys.exit(1)
 
     #data[column+' SENTENCES'] = [sent_tokenize(text) for text in data[column]]
     #data[column+' TOKENS'] = [word_tokenize(text) for text in data[column]]
